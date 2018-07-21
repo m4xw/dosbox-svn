65af39d2eb01b0c4d34d810c8e4c5e16488b5592
radius <andres.430@gmail.com>
2018-07-21 15:53:00 -0500

    cleanup makefiles

--- libretro/jni/Android.mk
+++ libretro/jni/Android.mk
@@ -1,6 +1,5 @@
 LOCAL_PATH := $(call my-dir)
-
-CORE_DIR := $(LOCAL_PATH)/../..
+CORE_DIR   := $(LOCAL_PATH)/../..
 
 INCFLAGS    := -I$(CORE_DIR)/libretro/deps/include/ -I$(CORE_DIR)/libretro/deps/include/SDL/
 COMMONFLAGS :=
@@ -25,21 +24,22 @@ endif
 include $(CORE_DIR)/libretro/Makefile.common
 
 SOURCES_C += \
-        $(CORE_DIR)/libretro/deps/SDL/src/thread/pthread/SDL_syscond.c \
-        $(CORE_DIR)/libretro/deps/SDL/src/thread/pthread/SDL_sysmutex.c \
-        $(CORE_DIR)/libretro/deps/SDL/src/thread/pthread/SDL_syssem.c \
-        $(CORE_DIR)/libretro/deps/SDL/src/thread/pthread/SDL_systhread.c \
-        $(CORE_DIR)/libretro/deps/SDL/src/thread/SDL_thread.c \
-        $(CORE_DIR)/libretro/deps/SDL/src/timer/unix/SDL_systimer.c \
-        $(CORE_DIR)/libretro/deps/SDL/src/timer/SDL_timer.c \
-        $(CORE_DIR)/libretro/deps/SDL/src/cdrom/dummy/SDL_syscdrom.c \
-        $(CORE_DIR)/libretro/deps/SDL/src/cdrom/SDL_cdrom.c \
-        $(CORE_DIR)/libretro/deps/SDL/src/SDL_error.c \
+    $(CORE_DIR)/libretro/deps/SDL/src/thread/pthread/SDL_syscond.c \
+    $(CORE_DIR)/libretro/deps/SDL/src/thread/pthread/SDL_sysmutex.c \
+    $(CORE_DIR)/libretro/deps/SDL/src/thread/pthread/SDL_syssem.c \
+    $(CORE_DIR)/libretro/deps/SDL/src/thread/pthread/SDL_systhread.c \
+    $(CORE_DIR)/libretro/deps/SDL/src/thread/SDL_thread.c \
+    $(CORE_DIR)/libretro/deps/SDL/src/timer/unix/SDL_systimer.c \
+    $(CORE_DIR)/libretro/deps/SDL/src/timer/SDL_timer.c \
+    $(CORE_DIR)/libretro/deps/SDL/src/cdrom/dummy/SDL_syscdrom.c \
+    $(CORE_DIR)/libretro/deps/SDL/src/cdrom/SDL_cdrom.c \
+    $(CORE_DIR)/libretro/deps/SDL/src/SDL_error.c \
 
 COMMONFLAGS += -D__LIBRETRO__ -DFRONTEND_SUPPORTS_RGB565 $(INCFLAGS) -DC_HAVE_MPROTECT="1"
 
 GIT_VERSION := " $(shell git rev-parse --short HEAD || echo unknown)"
 SVN_VERSION := " $(shell cat ../svn)"
+
 ifneq ($(GIT_VERSION)," unknown")
     COMMONFLAGS += -DGIT_VERSION=\"$(GIT_VERSION)\"
 endif
