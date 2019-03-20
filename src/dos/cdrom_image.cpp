b5a5ef323d10765860b3c2bb38caf98e2788fbb5
radius <andres.430@gmail.com>
2019-03-20 17:53:12 -0500

    added fake sdl support so this core can be used in consoles added switch target (tested) fixed dynarec for switch, thanks @m4xw fixed compilation for wiiu (tested) fixed compilation for emscripten (tested, starts now but crashes starting content, memory out of bounts)
    auto cycle detection seems to work fine on this one and not on @m4xw's updated port which is weird but in any case this core should work on all consoles now.
    an xbox og target would be nice, it should work with the fast old dynarec.

--- src/dos/cdrom_image.cpp
+++ src/dos/cdrom_image.cpp
@@ -421,8 +421,8 @@ bool CDROM_Interface_Image::CanReadPVD(TrackFile *file, int sectorSize, bool mod
 			(pvd[8] == 1 && !strncmp((char*)(&pvd[9]), "CDROM", 5) && pvd[14] == 1));
 }
 
-#if defined(WIN32)
-static string dirname(char * file) {
+#if defined(WIN32) || defined(HAVE_LIBNX) || defined(WIIU)
+static string FAKEdirname(char * file) {
 	char * sep = strrchr(file, '\\');
 	if (sep == NULL)
 		sep = strrchr(file, '/');
@@ -435,6 +435,7 @@ static string dirname(char * file) {
 		return tmp;
 	}
 }
+#define dirname FAKEdirname
 #endif
 
 bool CDROM_Interface_Image::LoadCueSheet(char *cuefile)
